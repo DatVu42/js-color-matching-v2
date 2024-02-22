ff789cfe0e034b10acd145ebbc4872f75fc279a1
Vu Dat <vudat42@gmail.com>
2024-02-22 09:42:10 +0700

    display color when click on each square

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { GAME_STATUS, PAIRS_COUNT } from './constants.js'
-import { getRandomColorPairs } from './utils.js';
+import { getColorElementList, getUlElement } from './selectors.js'
+import { getRandomColorPairs } from './utils.js'
 
 // Global variables
 let selections = []
@@ -12,4 +13,33 @@ let gameState = GAME_STATUS.PLAYING
 // 4. Add timer
 // 5. Handle replay click
 
-console.log(getRandomColorPairs(PAIRS_COUNT));
+function initColorList() {
+  const colorList = getRandomColorPairs(PAIRS_COUNT)
+  if (!colorList) return
+
+  const liList = getColorElementList()
+  liList.forEach((liElement, index) => {
+    const overlayElement = liElement.querySelector('.overlay')
+    if (overlayElement) overlayElement.style.backgroundColor = colorList[index]
+  })
+}
+
+function handleColorClick(liElement) {
+  if (!liElement) return
+  liElement.classList.add('active')
+}
+
+function attachEventForLiElement() {
+  const ulElement = getUlElement()
+  if (!ulElement) return
+  ulElement.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'LI') return
+
+    handleColorClick(event.target)
+  })
+}
+
+;(() => {
+  initColorList()
+  attachEventForLiElement()
+})()
